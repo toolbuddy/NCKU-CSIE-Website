ee2e4eff89c875e4c0872133204c05f4e8174fc5
Ziwei <k32367mdbf@yahoo.com.tw>
2018-03-04 15:57:15 +0800

    announce and resource

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require( 'express' );
-const config = require( './settings/server/config' );
+
+const pug = require( 'pug' );
+
+const config = require( './settings/server/config.js' );
+const routes = require( './routes/urls' );
+const announce = require('./routes/announce');
+const resource = require('./routes/resource');
 
 const server = express();
+
+server.listen( config.port );
+
+server.set( 'view engine', 'pug' );
+server.use( '/', routes );
+server.use('/announce', announce);
+server.use('/resource', resource);
 
