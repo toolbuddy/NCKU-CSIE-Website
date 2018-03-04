e2fdb27de08b9dd4d3618829c0b642d9a20c11b1
f74046234 <f74046234@ncku.edu.tw>
2018-03-04 17:29:09 +0800

    add research route / all

--- routes/research.js
+++ routes/research.js
@@ -7,4 +7,34 @@ router.get( '/', function(req, res) {
     );
 } );
 
+router.get( '/lab', function(req, res) {
+    res.render(
+        'research/lab'
+    );
+} );
+
+router.get( '/research_group', function(req, res) {
+    res.render(
+        'research/research_group'
+    );
+} );
+
+router.get( '/publication', function(req, res) {
+    res.render(
+        'research/publication'
+    );
+} );
+
+router.get( '/award', function(req, res) {
+    res.render(
+        'research/award'
+    );
+} );
+
+router.get( '/conference', function(req, res) {
+    res.render(
+        'research/conference'
+    );
+} );
+
 module.exports = router;
