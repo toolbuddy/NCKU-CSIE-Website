6dda3ffe99e972cc3ddad2ef6e1b7cf650c72b48
f26401004 <f26401004@gmail.com>
2018-03-04 16:52:36 +0800

    add commit.

--- routes/about.js
+++ routes/about.js
@@ -1,41 +1,49 @@
 const express = require( 'express' );
 const router = express.Router();
 
+// deal with the URL about. 
 router.get( '/', function( req, res ) {
     res.render(
             'about/index'
     );
 });
 
+// deal with the URL about/intro.
 router.get( '/intro', function( req, res ) {
     res.render(
             'about/intro'
     );
 });
 
+// deal with the URL about/teachers.
 router.get( '/teachers', function( req, res ) {
     res.render(
             'about/teachers'
     );
 });
 
+// deal with the URL about/members.
 router.get( '/members', function( req, res  ) {
     res.render(
             'about/members'
     );
 });
 
+// deal with the URL about/hornor.
 router.get( '/hornor', function( req, res ) {
     res.render(
             'about/hornor'
     );
 });
+
+// deal with the URL about/traffic.
 router.get( '/traffic', function( req, res ) {
     res.render(
             'about/traffic'
     );
 });
 
+// deal with the URL about/joinus.
 router.get( '/joinus', function( req, res ) {
     res.render(
             'about/joinus'
@@ -43,4 +51,3 @@ router.get( '/joinus', function( req, res ) {
 });
 
 module.exports = router;
-
--- routes/urls.js
+++ routes/urls.js
@@ -1,9 +1,13 @@
 const express = require( 'express' );
 
+
 const home = require( './home' );
+const about = require( './about' );
 
 const router = express.Router();
 
 router.use( '/', home );
+router.use( '/about', about );
+
 
 module.exports = router;
