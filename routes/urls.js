be2a7a4dedc7627b279c616704ed20f5c4cb8866
f74046234 <f74046234@ncku.edu.tw>
2018-03-04 16:54:40 +0800

    add route:research

--- routes/urls.js
+++ routes/urls.js
@@ -1,9 +1,11 @@
 const express = require( 'express' );
 
 const home = require( './home' );
+const research = require( './research' );
 
 const router = express.Router();
 
 router.use( '/', home );
+router.use( '/research', research );
 
 module.exports = router;
