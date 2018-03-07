99b43a5955f05054fb2ea8f10c581ac133153829
Ziwei <k32367mdbf@yahoo.com.tw>
2018-03-07 19:55:08 +0800

    WB測試

--- settings/webpack/config.js
+++ settings/webpack/config.js
@@ -5,7 +5,7 @@ const path = require( 'path' );
 // 取得所有文件
 let getEntry = ()=>{
     let entry = {};
-    for (let file of glob.sync(path.resolve('./views/**/*.js')))
+    for (let file of glob.sync(path.resolve('./static/src/*.js')))
     {
         entry[path.parse(file).name] = file
     }
@@ -19,15 +19,28 @@ module.exports =
     output:
     {
         path:       path.resolve('./static/dist'),
+        publicPath: '../dist/',
         filename:   '[name].js'
     },
-    // module:
-    // {
-    //     rules:
-    //     [{
-    //         test:    /\.js?$/,
-    //         // loader:  'babel-loader',
-    //         // options: { presets: [__dirname  + '/node_modules/babel-preset-es2015'] }
-    //     }]
-    // }
+    module: {
+    rules: [
+        {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        },
+        {
+            test: /\.(png|gif|jpe?g|svg|woff2?|ttf|eot)$/,
+            use: [
+                'file-loader',
+                {
+                    loader: 'url-loader',
+                    options: { limit: 8192 }
+                }
+            ]
+        },
+    ]}
 };
