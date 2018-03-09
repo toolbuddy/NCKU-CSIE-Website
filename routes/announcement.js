d87de0f8a76c546668476c31936e7ae278db690d
f74046234 <f74046234@ncku.edu.tw>
2018-03-10 00:41:19 +0800

    announcement query string basic

--- routes/announcement.js
+++ routes/announcement.js
@@ -6,35 +6,92 @@ const router = express.Router();
 
 // route to /announcement/all
 router.get( '/all', function( req, res, next ) {
-    var url = req.query;
-    if (url.tag === undefined)
-        next(new Error)
-        res.render( 'announcement/all' );
+	let obj_to_send = new Object();
+    
+	obj_to_send.page = req.query.page;
+	obj_to_send.announcement_id = req.query.announcement_id;
+	obj_to_send.tag = req.query.tag;
+	
+	if(obj_to_send.page == undefined) obj_to_send.page = 1;
+	
+	if(obj_to_send.announcement_id != undefined){
+		res.render( 'announcement/detail', { announcement_id: obj_to_send.announcement_id } );
+	}else{
+		res.render( 'announcement/all', obj_to_send );
+	}
     
 } );
 
 // route to /announcement/administrator
 router.get( '/administrator', function( req, res ) {
-    res.render( 'announcement/administrator' );
+	let obj_to_send = new Object();
+    
+	obj_to_send.page = req.query.page;
+	obj_to_send.announcement_id = req.query.announcement_id;
+	obj_to_send.tag = req.query.tag;
+	
+	if(obj_to_send.page == undefined) obj_to_send.page = 1;
+	
+	if(obj_to_send.announcement_id != undefined){
+		res.render( 'announcement/detail', { announcement_id: obj_to_send.announcement_id } );
+	}else{
+		res.render( 'announcement/administrator', obj_to_send );
+	}
 } );
 
 // route to /announcement/activity
 router.get( '/activity', function( req, res ) {
-    res.render( 'announcement/activity' );
+	let obj_to_send = new Object();
+    
+	obj_to_send.page = req.query.page;
+	obj_to_send.announcement_id = req.query.announcement_id;
+	obj_to_send.tag = req.query.tag;
+	
+	if(obj_to_send.page == undefined) obj_to_send.page = 1;
+	
+	if(obj_to_send.announcement_id != undefined){
+		res.render( 'announcement/detail', { announcement_id: obj_to_send.announcement_id } );
+	}else{
+		res.render( 'announcement/activity', obj_to_send );
+	}
 } );
 
 // route to /announcement/speech
 router.get( '/speech', function( req, res ) {
-    res.render( 'announcement/speech' );
+	let obj_to_send = new Object();
+    
+	obj_to_send.page = req.query.page;
+	obj_to_send.announcement_id = req.query.announcement_id;
+	obj_to_send.tag = req.query.tag;
+	
+	if(obj_to_send.page == undefined) obj_to_send.page = 1;
+	
+	if(obj_to_send.announcement_id != undefined){
+		res.render( 'announcement/detail', { announcement_id: obj_to_send.announcement_id } );
+	}else{
+		res.render( 'announcement/speech', obj_to_send );
+	}
 } );
 
 // route to /announcement/recruitment
 router.get( '/recruitment', function( req, res ) {
-    res.render( 'announcement/recruitment' );
+	let obj_to_send = new Object();
+    
+	obj_to_send.page = req.query.page;
+	obj_to_send.announcement_id = req.query.announcement_id;
+	obj_to_send.tag = req.query.tag;
+	
+	if(obj_to_send.page == undefined) obj_to_send.page = 1;
+	
+	if(obj_to_send.announcement_id != undefined){
+		res.render( 'announcement/detail', { announcement_id: obj_to_send.announcement_id } );
+	}else{
+		res.render( 'announcement/recruitment', obj_to_send );
+	}
 } );
 
 function errorHandler( err, req, res, next ) {
-    if ( res. )
+    //if ( res. )
 }
 
 module.exports = router;
