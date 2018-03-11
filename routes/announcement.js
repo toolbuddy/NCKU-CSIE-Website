d3b6bf41bcc1dc9b31b701b90d55791fb24b2f33
f26401004 <f26401004@gmail.com>
2018-03-11 13:05:08 +0800

    advanced routing of announcement: get the data_number & data to frontend.

--- routes/announcement.js
+++ routes/announcement.js
@@ -2,21 +2,38 @@
 
 const express = require( 'express' );
 const router = express.Router();
+const testDB = require( './testDB' );
 
 const query_handler = ( page_name ) => {
     return function(req, res, next){
-	    let obj_to_send = new Object();
-    
-	    obj_to_send.page = req.query.page;
-    	obj_to_send.announcement_id = req.query.announcement_id;
-    	obj_to_send.tag = req.query.tag;
-	
-	    if(obj_to_send.page == undefined) obj_to_send.page = 1;
-	
-    	if(obj_to_send.announcement_id != undefined){
-	    	res.render( 'announcement/detail', { announcement_id: obj_to_send.announcement_id } );
+
+	    let obj_to_send = {page: req.query.page, annoucement_id: req.query.announcement_id, tag: req.query.tag, data_number: 0};
+	    
+        if(obj_to_send.page == undefined) obj_to_send.page = 1;
+      
+        // tag correspond.
+        switch (page_name){
+            case '/all':
+                obj_to_send.tag = 1;
+                break;
+            case '/administrator':
+                obj_to_send.tag = 2;
+                break;
+            case '/activity':
+                obj_to_send.tag = 3;
+                break;
+            case '/speech':
+                obj_to_send.tag = 4;
+                break;
+            case '/recruitment':
+                obj_to_send.tag = 5;
+                break;
+        }
+        let data = {data_number: 1, content: testDB.find()};
+        if(obj_to_send.announcement_id != undefined){
+	        res.render( 'announcement/detail', { announcement_id: obj_to_send.announcement_id} );
     	}else{
-	    	res.render( 'announcement/' + page_name, obj_to_send );
+	        res.render( 'announcement/' + page_name, data);
     	}
     }    
 }
