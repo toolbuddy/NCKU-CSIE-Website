4a60c8a3662da3d08eb6659bc57a628c407fdf27
Darkborderman <reastw1234@gmail.com>
2018-03-18 21:49:31 +0800

    Rewrite and finish linting comment (#17)
    * ==Update json to js==
    
    * ==Add eslint rules comment==
    Add eslint comment and fix some linting
    
    * ==Initialize pug-lint==
    Install pug-lint and initialized.
    However the rule is not very detailed.
    
    * ==Finish Linting rules==

--- routes/research.js
+++ routes/research.js
@@ -1,29 +1,29 @@
 // router for /research
 const express = require( 'express' );
-const router = express.Router();
+const router = new express.Router();
 
 // route to /research/labs
-router.get( '/labs', function( req, res ) {
+router.get( '/labs', function ( req, res ) {
     res.render( 'research/labs' );
 } );
 
 // route to /research/groups
-router.get( '/groups', function( req, res ) {
+router.get( '/groups', function ( req, res ) {
     res.render( 'research/groups' );
 } );
 
 // route to /research/publications
-router.get( '/publications', function( req, res ) {
+router.get( '/publications', function ( req, res ) {
     res.render( 'research/publications' );
 } );
 
 // route to /research/awards
-router.get( '/awards', function( req, res ) {
+router.get( '/awards', function ( req, res ) {
     res.render( 'research/awards' );
 } );
 
 // route to /research/conferences
-router.get( '/conferences', function( req, res ) {
+router.get( '/conferences', function ( req, res ) {
     res.render( 'research/conferences' );
 } );
 
