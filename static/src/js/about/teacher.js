f83428eda80718ae257c0f1b814609ff83ecf9a4
Ziwei <k32367mdbf@yahoo.com.tw>
2018-03-18 13:18:26 +0800

    upd

--- static/src/js/about/teacher.js
+++ static/src/js/about/teacher.js
@@ -1,1 +1,7 @@
 require('../../sass/about/teacher.sass')
+
+window.addEventListener('scroll', e=>{
+    let grid = document.querySelector('#grid')
+    // grid.style.gridTemplateRows = '100px auto'
+    console.log('RRR')
+})
