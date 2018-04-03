3b017f6072b95216f797b776c3c4f2161463a7b5
wang0630 <j2081499@gmail.com>
2018-04-03 11:57:59 +0800

    original

--- static/dist/script.js
+++ static/dist/script.js
@@ -3,10 +3,10 @@ var UIController = (function () {
   setEventHandler = () => {
 
     // check if the cursor actually enter a div or not
-    var isEnterDiv = false;
+    //var isEnterDiv = false;
     // select parent element which is the overview page
     var hoverOnCardParent = document.querySelector(".overview");
-    var hoverOnCardDiv = document.querySelectorAll("#hover-event");
+    //var hoverOnCardDiv = document.querySelectorAll("#hover-event");
     
     // toggle function
     var displayToFlex = function (e) {
@@ -16,12 +16,12 @@ var UIController = (function () {
         e.target.querySelector("#hover-event").style.display = "flex";
       }
     }
-
+    /*
     var checkEnterBlock = (e) => {
       isEnterDiv=true;
       console.log(isEnterDiv);
     }
-
+    */
     //var displayToNone
     // set mouseover event to the div element
     // cuz it appears on top of the overview__hover
@@ -30,24 +30,19 @@ var UIController = (function () {
     var displayToNone = function (e) {
       console.log("I hover out");
       console.log(e.target);
-      if (isEnterDiv) {
-        if(e.target.id === "hover-event"){
+      
+       if(e.target.id === "hover-event"){
           e.target.style.display="none";
-          isEnterDiv=false;
-        }
-      }
-      else{
-        if(e.target.classList.contains("overview__hover")){
-          e.target.querySelector("#hover-event").style.display = "none";
-        }
       }
     }
 
 
     // set event handler
+    /*
     hoverOnCardDiv.forEach((cur) => {
       cur.addEventListener("mouseenter", checkEnterBlock);
     });
+    */
     hoverOnCardParent.addEventListener("mouseover", displayToFlex);
     hoverOnCardParent.addEventListener("mouseout", displayToNone);
   }
