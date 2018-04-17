c685788d8aebae269e121dad19a269800d43b6f7
Ziwei <k32367mdbf@yahoo.com.tw>
2018-04-17 20:24:21 +0800

    upd

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ const routes = require( './routes/urls' );
 
 // start server
 const server = express();
-<<<<<<< HEAD
-
-server.use( express.static( __dirname + '/static/dist' ) );
-
-=======
->>>>>>> 1ac65bc6c7a62ae3705b04005005a9ad5257bb26
 server.listen( config.port );
 
 // set render engine
