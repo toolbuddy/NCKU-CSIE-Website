c9c9093ec33ea14ee9aabe64647513a21fa67060
Darkborderman <reastw1234@gmail.com>
2018-04-30 22:00:07 +0800

    ==Update puglint and stylelint rules==

--- settings/lint/stylelint/stylelint.js
+++ settings/lint/stylelint/stylelint.js
@@ -1,5 +1,86 @@
 module.exports = {
-    "rules": {
-        "comment-no-empty": true,
-    }
-}
+    'rules': {
+
+        /* Possible Errors category*/
+        // no empty comment
+        'comment-no-empty': true,
+
+        // non invail hex color
+        'color-no-invaild-hex': true,
+
+        // no duplicate font names
+        'font-family-no-duplicate-names': true,
+
+        // no missing generic families in lists of font family names
+        'font-family-no-missing-generic-family-keyword': true,
+
+        // need space before and after each operator
+        'function-calc-no-unspaced-operator': true,
+
+        // linear-gradient need to specify color direction
+        'function-linear-gradient-no-nonstandard-direction': true,
+
+        // string need to use \A to start a new line
+        'string-no-newline': true,
+
+        // no unknown unit
+        'unit-no-unknown': true,
+
+        // no unknown property
+        'property-no-unknown': true,
+
+        // no !important in keyframe
+        'keyframe-declaration-no-important': true,
+
+        // no duplicate properties in selectors
+        'declaration-block-no-duplicate-properties': true,
+
+        // specify properties override order(in one selector)
+        'declaration-block-no-shorthand-property-overrides': true,
+
+        // no empty block
+        'block-no-empty': true,
+
+        // no unknown pseudo selector class
+        'selector-pseudo-class-no-unknown': true,
+
+        // no unknown pseudo selector element
+        'selector-pseudo-element-no-unknown': true,
+
+        // no unknown selector
+        'selector-type-no-unknown': true,
+
+        // no unknown @media
+        'media-feature-name-no-unknown': true,
+
+        // no unknown @rule
+        'at-rule-no-unknown': true,
+
+        // no empty comment
+        'comment-no-empty': true,
+
+        // specifty properties override order(multiple selectors)
+        'no-descending-specificity': true,
+
+        // no duplicate import files
+        'no-duplicate-at-import-rules': true,
+
+        // no duplicate selectors
+        'no-duplicate-selectors': true,
+
+        // no empty source file
+        'no-empty-source': true,
+
+        // no extra semicolons
+        'no-extra-semicolons': true,
+
+        // no double slash comment(//) for CSS files
+        'no-invalid-double-slash-comments': true,
+
+        /* Limit language features category*/
+
+        /* Stylistic issues category*/
+
+
+    },
+};
