df43fe8d7b26d88145a024294c5e47eacd9739d4
Kevin <ProFatXuanAll@gmail.com>
2018-05-11 16:16:22 +0800

    sequelize auto build table

---
+++ settings/database/sequelize_auto.js
@@ -0,0 +1,22 @@
+const path = require( 'path' );
+const SequelizeAuto = require('sequelize-auto')
+const projectRoot = path.dirname( path.dirname( __dirname ) );
+const config = require( `${ projectRoot }/settings/database/config` );
+
+const auto = new SequelizeAuto(
+    'teacher',
+    config.username,
+    config.password,
+    {
+        host: config.host,
+        dialect:  'mysql',
+        directory: `${ projectRoot }/models/teacher/tables`,
+        port: config.port,
+        additional: {
+            timestamps: false,
+    },
+} );
+
+auto.run( (err) => {
+    if (err) throw err;
+} );
