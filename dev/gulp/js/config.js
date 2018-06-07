8857b531871074e075308fc894bf0410b587ce18
ProFatXuanAll <ProFatXuanAll@gmail.com>
2018-06-07 22:54:50 +0800

    fix css nested include

--- dev/gulp/js/config.js
+++ dev/gulp/js/config.js
@@ -4,11 +4,11 @@ const deepFreeze = require( `${ projectRoot }/lib/deep-freeze` );
 
 /**
  * @constant
- * @readonly {string[]} config.build.src  - Array of glob of source files for building Frontend JavaScript.
- * @readonly {string}   config.build.dest - Glob of destination directory for building Frontend JavaScript.
- * @readonly {string}   config.lint.rule  - Glob of lint rule file for linting Frontend JavaScript.
- * @readonly {string[]} config.lint.src   - Array of glob of source files for linting Frontend JavaScript.
- * @readonly {string}   config.lint.dest  - Glob of destination directory for linting Frontend JavaScript.
+ * @readonly {string[]} config.build.src  - Array of glob of source files for building frontend ECMAScript.
+ * @readonly {string}   config.build.dest - Glob of destination directory for building frontend ECMAScript.
+ * @readonly {string}   config.lint.rule  - Glob of lint rule file for linting frontend ECMAScript.
+ * @readonly {string[]} config.lint.src   - Array of glob of source files for linting frontend ECMAScript.
+ * @readonly {string}   config.lint.dest  - Glob of destination directory for linting frontend ECMAScript.
  */
 
 const config = {
