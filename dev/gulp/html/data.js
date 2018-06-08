e92ba81c6012c49092bb758b6fe93f64629e3a51
ProFatXuanAll <ProFatXuanAll@gmail.com>
2018-06-08 22:18:49 +0800

    Feature nested include (#36)
    * fix duplicated import in scss
    
    * add nest extend layout in pug
    
    * add static file path mixin in pug

--- dev/gulp/html/data.js
+++ dev/gulp/html/data.js
@@ -1,102 +1,103 @@
 const path = require( 'path' );
 const projectRoot = path.dirname( path.dirname( path.dirname( __dirname ) ) );
+const { staticUrl, } = require( `${ projectRoot }/settings/server/config` );
 const getTeachersProfile = require( `${ projectRoot }/models/teacher/operation/get-teachers-profile` );
 
 const data = {
     about: {
         async honor ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async intro ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async location ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async members ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async teacher ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async teachers ( language ) {
-            return { language, teachers: await getTeachersProfile( language ), };
+            return { language, staticUrl, teachers: await getTeachersProfile( language ), };
         },
     },
     announcement: {
         async activity ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async administrator ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async all ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async recruitment ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async speech ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
     },
     home: {
         async index ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
     },
     research: {
         async awards ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async conferences ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async groups ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async labs ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async publications ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
     },
     resource: {
         async fix ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async ieet ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async law ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async rent ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async resources ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
     },
     student: {
         async college ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async course ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async international ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async master ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async phd ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
         async scholarship ( language ) {
-            return { language, };
+            return { language, staticUrl, };
         },
     },
 };
