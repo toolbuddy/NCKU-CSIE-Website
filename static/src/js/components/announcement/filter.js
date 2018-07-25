31d0bc513f71ca776bfe100af87972a9c6798c75
yasin <luoyasin@gmail.com>
2018-07-25 13:08:16 +0800

    finished filter layout and filter.js(the relation of tag--all and the other)

--- static/src/js/components/announcement/filter.js
+++ static/src/js/components/announcement/filter.js
@@ -22,15 +22,31 @@
 
 // Construct filter tags' UI
 export default function filterTags ( defaultTag = null ) {
-    document.getElementById( 'filter__tags' ).childNodes.forEach( ( tag ) => {
-        tag.onclick = () => {
-            if ( tag.classList.contains( 'tags__tag--active' ) )
-                tag.classList.remove( 'tags__tag--active' );
-            else
-                tag.classList.add( 'tags__tag--active' );
-        };
-    } );
-
-    if ( defaultTag )
+    const filterTags = document.getElementById( 'filter__tags' ).childNodes;
+    const tagsTagAll = document.getElementById( 'tags__tag--all' );
+    if ( filterTags.length === 1 )
         document.getElementById( `tags__tag--${ defaultTag }` ).classList.add( 'tags__tag--active' );
+    else {
+        filterTags.forEach( ( tag ) => {
+            tag.onclick = () => {
+                if ( tag.classList.contains( 'tags__tag--active' ) )
+                    tag.classList.remove( 'tags__tag--active' );
+
+                else if ( tag.classList.contains( 'tags__tag--all' ) ) {
+                    tag.classList.add( 'tags__tag--active' );
+                    filterTags.forEach( ( tag ) => {
+                        if ( !tag.classList.contains( 'tags__tag--all' ) )
+                            tag.classList.remove( 'tags__tag--active' );
+                    } );
+                }
+                else {
+                    tagsTagAll.classList.remove( 'tags__tag--active' );
+                    tag.classList.add( 'tags__tag--active' );
+                }
+            };
+        } );
+
+        if ( defaultTag )
+            document.getElementById( `tags__tag--${ defaultTag }` ).classList.add( 'tags__tag--active' );
+    }
 }
