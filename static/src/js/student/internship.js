1e0f97fbc9fdec35ff840cde8a2ef2b2b1bf5ad6
kaeteyaruyo <kaeteyaruyo@gmail.com>
2018-08-07 15:45:23 +0800

    add filter on every page

--- static/src/js/student/internship.js
+++ static/src/js/student/internship.js
@@ -3,3 +3,6 @@
  */
 /* eslint no-unused-vars: off */
 import style from 'static/dist/css/student/internship.min.css';
+import { singleDefaultTagFilter, } from 'static/src/js/components/announcement/filter/index.js';
+
+singleDefaultTagFilter( 'internship' );
