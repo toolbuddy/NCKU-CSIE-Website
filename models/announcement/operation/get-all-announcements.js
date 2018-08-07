d6e52c812aa62ba300a8a4ae3546c5c3f05e136b
yasin <luoyasin@gmail.com>
2018-08-07 21:22:47 +0800

    q

--- models/announcement/operation/get-all-announcements.js
+++ models/announcement/operation/get-all-announcements.js
@@ -3,7 +3,8 @@ const Op = require( 'sequelize' ).Op;
 const projectRoot = path.dirname( path.dirname( path.dirname( __dirname ) ) );
 const opRoot = path.resolve( projectRoot, 'models/announcement/operation' );
 const associations = require( path.resolve( opRoot, 'associations' ) );
-const validate = require( path.resolve( opRoot, 'validate' ) );
+
+// Const validate = require( path.resolve( opRoot, 'validate' ) );
 const defaultValue = require( path.resolve( projectRoot, 'settings/default-value/announcement/config' ) );
 
 module.exports = async ( {
--- static/src/js/components/announcement/filter/render.js
+++ static/src/js/components/announcement/filter/render.js
@@ -2,17 +2,23 @@ import briefing from 'static/src/pug/components/announcement/briefing.pug';
 import { pageOnClick, } from 'static/src/js/components/announcement/filter/event.js';
 import { timeFormating, }  from 'static/src/js/components/announcement/filter/format.js';
 
-// Construct filter tags' UI
-export function renderFilter ( defaultTagName = null ) {
-    let flip = 0;
+// Construct filter's UI
+export function renderFilter ( defaultTagName = 'all' ) {
+    let toggle = true;
     const buttonIcon = document.getElementById( 'button__icon' );
     const filterTags = document.getElementById( 'filter__tags' );
     const filterTime = document.getElementById( 'filter__time' );
     const defaultTag = document.getElementById( `tags__tag--${ defaultTagName }` );
 
+    /**
+     * When `filter__button` is clicked, `filter__tags` and `filter__time` will be in either state:
+     *     * hidden, add class `tags--hidden` and `time--hidden` on `filter__tags` and `filter__time` respectively.
+     *     * show, remove class `tags--hidden` and `time--hidden` on `filter__tags` and `filter__time` respectively.
+     */
+
     document.getElementById( 'filter__button' ).addEventListener( 'click', () => {
-        flip = ( flip + 1 ) % 2;
-        if ( flip ) {
+        if ( toggle ) {
+            // TODO: change `tags--hidden` and `time--hidden` to `filter__tags--hidden` and `filter__time--hidden`
             filterTags.classList.remove( 'tags--hidden' );
             filterTime.classList.remove( 'time--hidden' );
             buttonIcon.classList.add( 'button__icon--active' );
@@ -22,48 +28,69 @@ export function renderFilter ( defaultTagName = null ) {
             filterTime.classList.add( 'time--hidden' );
             buttonIcon.classList.remove( 'button__icon--active' );
         }
+        toggle = !toggle;
     } );
-    const query = new URLSearchParams( window.history.location );
-    if ( !query.getAll( 'tags' ).length )
-        defaultTag.classList.add( 'tags__tag--active' );
-    if ( defaultTagName === 'all' || defaultTagName === 'activity' ) {
-        filterTags.forEach( ( tag ) => {
-            tag.onclick = () => {
-                if ( tag.classList.contains( 'tags__tag--active' ) )
-                    tag.classList.remove( 'tags__tag--active' );
 
-                else if ( tag.classList.contains( `tags__tag--${ defaultTagName }` ) ) {
-                    tag.classList.add( 'tags__tag--active' );
-                    filterTags.forEach( ( tag ) => {
-                        if ( !tag.classList.contains( `tags__tag--${ defaultTagName }` ) )
-                            tag.classList.remove( 'tags__tag--active' );
-                    } );
-                }
-                else {
-                    defaultTag.classList.remove( 'tags__tag--active' );
-                    tag.classList.add( 'tags__tag--active' );
-                }
-            };
+    /**
+     * Activate `tags__tag--{ defaultTagName }` if:
+     *     * it is in a single default tag filter
+     *     * there is no tags in URL.
+     *     * there is no valid tags in URL.
+     *
+     * Tags can be validated through `document.getElementById()` if:
+     *     * tag is exist, it will return DOM object
+     *     * tag is not exist, it will return `undefined`
+     */
+
+    defaultTag.classList.add( 'tags__tag--active' );
+
+    const query = new URLSearchParams( window.location.search );
+    const currentTags = query.getAll( 'tags' );
+    let validTagCount = 0;
+    if ( currentTags.length ) {
+        currentTags.forEach( ( tag ) => {
+            const tagObj = document.getElementById( `tags__tag--${ tag }` );
+
+            // Tag validation
+            if ( tagObj ) {
+                tagObj.classList.add( 'tags__tag--active' );
+                validTagCount += 1;
+            }
         } );
     }
-    else {
-        filterTags.forEach( ( tag ) => {
-            tag.onclick = () => {
-                if ( !tag.classList.contains( `tags__tag--${ defaultTagName }` ) ) {
-                    if ( tag.classList.contains( 'tags__tag--active' ) )
-                        tag.classList.remove( 'tags__tag--active' );
-                    else
-                        tag.classList.add( 'tags__tag--active' );
-                }
-                else {
-                    filterTags.forEach( ( tag ) => {
-                        if ( !tag.classList.contains( `tags__tag--${ defaultTagName }` ) )
-                            tag.classList.remove( 'tags__tag--active' );
-                    } );
-                }
-            };
+    if ( validTagCount && defaultTagName === 'all' )
+        defaultTag.classList.remove( 'tags__tag--active' );
+
+    const tagOnClick = defaultTagName === 'all' ?
+        ( event ) => {
+            if ( event.target.classList.contains( 'tags__tag--active' ) )
+                event.target.classList.remove( 'tags__tag--active' );
+            else
+                event.target.classList.add( 'tags__tag--active' );
+
+            if ( defaultTag.classList.contains( 'tags__tag--active' ) )
+                defaultTag.classList.remove( 'tags__tag--active' );
+        } :
+        ( event ) => {
+            if ( event.target.classList.contains( 'tags__tag--active' ) )
+                event.target.classList.remove( 'tags__tag--active' );
+            else
+                event.target.classList.add( 'tags__tag--active' );
+        };
+
+    const defaultTagOnClick = () => {
+        filterTags.childNodes.forEach( ( tag ) => {
+            if ( tag.classList.contains( 'tags__tag--active' ) )
+                tag.classList.remove( 'tags__tag--active' );
         } );
-    }
+
+        if ( !defaultTag.classList.contains( 'tags__tag--active' ) )
+            defaultTag.classList.add( 'tags__tag--active' );
+    };
+
+    filterTags.childNodes.forEach( tag => tag.addEventListener( 'click', tagOnClick ) );
+    defaultTag.removeEventListener( 'click', tagOnClick );
+    defaultTag.addEventListener( 'click', defaultTagOnClick );
 }
 
 const pages = document.getElementById( 'pages' );
