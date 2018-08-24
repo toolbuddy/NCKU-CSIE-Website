89b93cce032aba300014a3e9a4b779a3fb986e5d
kaeteyaruyo <kaeteyaruyo@gmail.com>
2018-08-25 02:15:36 +0800

    add router in home.js

--- routes/home.js
+++ routes/home.js
@@ -8,4 +8,20 @@ router.get( '/', ( req, res ) => {
     res.sendFile( `${ global.projectRoot }/static/dist/html/home/index.${ req.query.language }.html` );
 } );
 
+// Route to login page
+router.get( '/login', ( req, res ) => {
+    res.sendFile( `${ global.projectRoot }/static/dist/html/home/index.${ req.query.language }.html` );
+} );
+
+// Route to search page
+router.get( '/search', ( req, res ) => {
+    res.sendFile( `${ global.projectRoot }/static/dist/html/home/index.${ req.query.language }.html` );
+} );
+
+// Route to calender page
+router.get( '/calendar', ( req, res ) => {
+    res.sendFile( `${ global.projectRoot }/static/dist/html/home/index.${ req.query.language }.html` );
+} );
+
+
 module.exports = router;
