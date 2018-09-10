f4a38d0000c066404f35f54134d52282e0960e4e
IsabelTseng <isabel870310@gmail.com>
2018-09-10 21:22:07 +0800

    finish filter, haven't cr

--- static/src/js/components/about/faculty/filters.js
+++ static/src/js/components/about/faculty/filters.js
@@ -1,7 +1,36 @@
 function filterSelection () {
+    // Get filters
+    const filters = [];
     Array.from( document.getElementsByClassName( 'filters__filter' ) ).forEach( ( filter ) => {
         if ( filter.classList.contains( 'filters__filter--active' ) )
-            document.getElementsByClassName( 'filters__filter' );
+            filters.push( filter.getAttribute( 'data' ) );
+    } );
+
+    // Start to filter
+    Array.from( document.getElementsByClassName( 'card' ) ).forEach( ( card ) => {
+        // Show all carsd at first
+        card.classList.remove( 'card--hide' );
+
+        // Hide cards which are not same to filters
+        Array.from( card.getElementsByClassName( 'departments' ) ).forEach( ( departments ) => {
+            // Deal with cards with department tags
+            if ( departments.childNodes.length ) {
+                // Get id of department tags
+                const cardTags = [];
+                Array.from( card.getElementsByClassName( 'departments__department' ) ).forEach( ( department ) => {
+                    cardTags.push( department.getAttribute( 'data' ) );
+                } );
+                filters.forEach( ( filterTag ) => {
+                    // Hide card which are not same to filters
+                    if ( cardTags.includes( filterTag ) === false )
+                        card.classList.add( 'card--hide' );
+                } );
+            }
+
+            // Deal with cards without department tags
+            else
+                card.classList.add( 'card--hide' );
+        } );
     } );
 }
 
