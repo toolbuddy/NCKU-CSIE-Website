8a85e12548f8e2eee8f4cdd80fc41ea79fd702db
IsabelTseng <isabel870310@gmail.com>
2018-09-12 00:47:37 +0800

    add no-request text, haven't cr

--- static/src/js/components/about/faculty/filters.js
+++ static/src/js/components/about/faculty/filters.js
@@ -1,6 +1,23 @@
+function showNoResult ( noResult ) {
+    // Show 404
+    if ( noResult ) {
+        Array.from( document.getElementsByClassName( 'no-result__text' ) ).forEach( ( noResultText ) => {
+            noResultText.classList.remove( 'no-result__text--hide' );
+        } );
+    }
+
+    // Hide 404 at first
+    else {
+        Array.from( document.getElementsByClassName( 'no-result__text' ) ).forEach( ( noResultText ) => {
+            noResultText.classList.add( 'no-result__text--hide' );
+        } );
+    }
+}
 function filterSelection () {
     // Get filters
     const filters = [];
+    let noResult = false;
+    showNoResult( noResult );
     Array.from( document.getElementsByClassName( 'filters__filter' ) ).forEach( ( filter ) => {
         if ( filter.classList.contains( 'filters__filter--active' ) )
             filters.push( filter.getAttribute( 'data' ) );
@@ -24,6 +41,8 @@ function filterSelection () {
                     // Hide card which are not same to filters
                     if ( cardTags.includes( filterTag ) === false )
                         card.classList.add( 'card--hide' );
+                    else
+                        noResult = true;
                 } );
             }
 
@@ -32,6 +51,7 @@ function filterSelection () {
                 card.classList.add( 'card--hide' );
         } );
     } );
+    showNoResult( noResult );
 }
 
 Array.from( document.getElementsByClassName( 'filters__filter' ) ).forEach( filter => filter.addEventListener( 'click', function changeColor ( e ) {
