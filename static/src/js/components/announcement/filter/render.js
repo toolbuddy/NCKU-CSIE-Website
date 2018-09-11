8f7ea7d6d7457701dec4bcf3720d05c6b2b438b5
yasin <luoyasin@gmail.com>
2018-09-11 09:36:50 +0800

    2nd version of pagination, media query not yet

--- static/src/js/components/announcement/filter/render.js
+++ static/src/js/components/announcement/filter/render.js
@@ -68,30 +68,29 @@ export function renderPage () {
 
         if ( page.innerHTML === currentPage )
             page.classList.add( 'pages__page--active' );
-        if ( page.innerHTML < currentPage - 2 || page.innerHTML > Number.parseInt( currentPage, 10 ) + 2 )
+        else if ( page.innerHTML < currentPage - 2 || page.innerHTML > Number.parseInt( currentPage, 10 ) + 2 )
             page.setAttribute( 'style', 'display: none;' );
 
-        const extraItem1 = document.createElement( 'button' );
-        extraItem1.className = 'pages__extra';
-        extraItem1.id = 'pages__extra1';
-        extraItem1.innerHTML = '...';
-        const extraItem2 = document.createElement( 'button' );
-        extraItem2.className = 'pages__extra';
-        extraItem2.id = 'pages__extra2';
-        extraItem2.innerHTML = '...';
-        if ( index === 0 )
+        if ( index === 0 || index === array.length - 1 )
             page.removeAttribute( 'style', 'display: none;' );
 
         if ( index === array.length - 1 ) {
-            page.removeAttribute( 'style', 'display: none;' );
-            if ( document.getElementById( 'pages__extra2' ) )
-                document.getElementById( 'pages__extra2' ).parentNode.removeChild( document.getElementById( 'pages__extra2' ) );
+            const extraItem1 = document.createElement( 'button' );
+            extraItem1.className = 'pages__extra';
+            extraItem1.id = 'pages__extra1';
+            extraItem1.innerHTML = '...';
+            const extraItem2 = document.createElement( 'button' );
+            extraItem2.className = 'pages__extra';
+            extraItem2.id = 'pages__extra2';
+            extraItem2.innerHTML = '...';
             if ( document.getElementById( 'pages__extra1' ) )
                 document.getElementById( 'pages__extra1' ).parentNode.removeChild( document.getElementById( 'pages__extra1' ) );
-            if ( Array.from( document.getElementsByClassName( 'pages__page' ) )[ index - 1 ].hasAttribute( 'style' ) )
-                pages.insertBefore( extraItem2, pages.childNodes[ index + 1 ] );
+            if ( document.getElementById( 'pages__extra2' ) )
+                document.getElementById( 'pages__extra2' ).parentNode.removeChild( document.getElementById( 'pages__extra2' ) );
             if ( Array.from( document.getElementsByClassName( 'pages__page' ) )[ 1 ].hasAttribute( 'style' ) )
                 pages.insertBefore( extraItem1, pages.childNodes[ 2 ] );
+            if ( Array.from( document.getElementsByClassName( 'pages__page' ) )[ index - 1 ].hasAttribute( 'style' ) )
+                pages.insertBefore( extraItem2, pages.childNodes[ index + 1 ] );
         }
     } );
 }
