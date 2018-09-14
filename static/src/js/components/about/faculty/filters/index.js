69f7aa6b4dd8fbb87585c94b38d5e2052ab72283
ProFatXuanAll <ProFatXuanAll@gmail.com>
2018-09-14 14:26:26 +0800

    Feature fix (#44)
    * fix scss in /about/faculty-detail.scss
    
    * add comment

--- static/src/js/components/about/faculty/filters/index.js
+++ static/src/js/components/about/faculty/filters/index.js
@@ -1,9 +1,21 @@
 import { registClick, registFilter, } from 'static/src/js/components/about/faculty/filters/event.js';
 
+/**
+ * Construct filter's events on DOM element `filters`, 'cards`, `noResult`.
+ *
+ * @param {HTMLElement} filters
+ * @param {HTMLElement} cards
+ * @param {HTMLElement} noResult
+ *
+ * Regist event `registClick` for each DOM element with class nmae `.filters__filter`.
+ * Regist event `registFilter` for each DOM element with class nmae `.filters__filter`
+ * which affect DOM elements `filters`, `cards`, and `noResult`.
+ */
+
 export default ( filters, cards, noResult ) => {
     filters = Array.from( filters.getElementsByClassName( 'filters__filter' ) );
     filters.forEach( ( filter ) => {
-        registClick( filter, cards, noResult );
+        registClick( filter );
         registFilter( filter, filters, cards, noResult );
     } );
 };
--- static/src/js/components/about/faculty/filters/render.js
+++ static/src/js/components/about/faculty/filters/render.js
@@ -1,78 +1,101 @@
+/**
+ * Render when click on `filter`.
+ *
+ * @param {HTMLElement} filter
+ *
+ * Toggle filter's class `filters__filter--active` on and off.
+ * Active filter if filter contains class `filters__filter--active`,
+ * deactive filter otherwise.
+ */
+
 export function renderFilter ( filter ) {
-    // Toggle filter.
     if ( filter.classList.contains( 'filters__filter--active' ) )
         filter.classList.remove( 'filters__filter--active' );
     else
         filter.classList.add( 'filters__filter--active' );
 }
 
+/**
+ * Render when click on DOM element with class name `filters__filter`.
+ *
+ * @param {HTMLElement[]} filters
+ * @param {HTMLElement}   cards
+ * @param {HTMLElement}   noResult
+ *
+ * Filter on all `cards__card`.
+ * Passed test when:
+ *     * No filter condition is presented.
+ *     * Test target existes and contains all filter's conditions.
+ * Failed test when:
+ *     * Test target does not exist.
+ *     * Test target existes and lacks at least one filter's conditions.
+ *
+ * If all `cards__card` failed test, present `noResult`.
+ * Hide `noResult` otherwise.
+ */
+
 export function renderCards ( filters, cards, noResult ) {
-    // Get actived filters.
+    // Get filter's conditions.
     const selectedFilters = filters
     .filter( filter => filter.classList.contains( 'filters__filter--active' ) )
     .map( filter => filter.getAttribute( 'data' ) );
 
-    // If no filter in actived.
+    // If no filter condition is presented.
     if ( selectedFilters.length === 0 ) {
-        // Show all cards
+        // Show all `cards__card`.
         Array.from( cards.getElementsByClassName( 'cards__card' ) )
         .forEach( ( card ) => {
             if ( card.classList.contains( 'card--hide' ) )
                 card.classList.remove( 'card--hide' );
         } );
 
-        // Hide no-result section
+        // Hide `no-result`.
         if ( !noResult.classList.contains( 'no-result--hide' ) )
             noResult.classList.add( 'no-result--hide' );
         return;
     }
 
-    // If there is turned-on filter
-    // assume all cards are hidden at first
+    // Assuming that all `cards__card` failed on test.
     let noShowedCard = true;
 
-    // Start to filter
+    // Filter on all `cards__card`.
+    // If `cards__card` does not pass the filter's test, hide it.
+    // Otherwise `cards__card` remains showing.
     Array.from( cards.getElementsByClassName( 'cards__card' ) ).forEach( ( card ) => {
-        // Get tags of the card
+        // Get testing target from `cards__card`.
         const departments = Array.from( card.getElementsByClassName( 'departments__department' ) )
         .map( department => department.getAttribute( 'data' ) )
         .filter( data => data != null );
 
-        // Deal with cards which have tags
+        // Filter on test target.
         if ( departments.length ) {
-            // If tags do not match filters
+            // Test failed.
             if ( selectedFilters.some( filter => departments.indexOf( filter ) < 0 ) ) {
-                // Hide the card
                 if ( !card.classList.contains( 'card--hide' ) )
                     card.classList.add( 'card--hide' );
             }
 
-            // If tags match filters
+            // Test passed.
             else {
-                // There is at least one card showed
                 noShowedCard = false;
 
-                // Show the card
                 if ( card.classList.contains( 'card--hide' ) )
                     card.classList.remove( 'card--hide' );
             }
         }
 
-        // Deal with cards which do not have tags
+        // Test failed.
         else if ( !card.classList.contains( 'card--hide' ) )
             card.classList.add( 'card--hide' );
     } );
 
-    // No card is showed
+    // All `cards__card` failed test.
     if ( noShowedCard ) {
-        // Show no-result tag
         if ( noResult.classList.contains( 'no-result--hide' ) )
             noResult.classList.remove( 'no-result--hide' );
     }
 
-    // At least one card is showed
+    // Exist some `cards__card` passed test.
     else if ( !noResult.classList.contains( 'no-result--hide' ) )
-
-        // Hide no-result tag
         noResult.classList.add( 'no-result--hide' );
 }
