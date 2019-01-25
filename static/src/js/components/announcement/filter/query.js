6e71a02c0b6e4292c0766422994e984f6b830d09
Kevin <ProFatXuanAll@gmail.com>
2019-01-25 15:49:41 +0800

    fix icon path

--- static/src/js/components/announcement/filter/query.js
+++ static/src/js/components/announcement/filter/query.js
@@ -291,7 +291,7 @@ export const multipleDefaultTags = {
      */
 
     getAllAnnouncements () {
-        const { from, to, page, languageId, amount, } = QueryString.getFilters( null );
+        const { from, to, page, languageId, } = QueryString.getFilters( null );
         const query = QueryString.generate( {
             //tags:     multipleDefaultTags.defaultTag,
             tags: [],
