57614017e806b5b5911c7bdc353cf455384783c6
Kevin <ProFatXuanAll@gmail.com>
2019-02-13 16:50:17 +0800

    add hot announcement op

--- apis/announcement.js
+++ apis/announcement.js
@@ -6,6 +6,7 @@ import getAllPages from 'models/announcement/operations/get-all-pages.js';
 import getAnnouncement from 'models/announcement/operations/get-announcement.js';
 import getAllPinnedAnnouncements from 'models/announcement/operations/get-all-pinned-announcements.js';
 import getAnnouncementsByTags from 'models/announcement/operations/get-announcements-by-tags.js';
+import getHotAnnouncements from 'models/announcement/operations/get-hot-announcements.js';
 import getPagesByTags from '../models/announcement/operations/get-pages-by-tags';
 import getPinnedAnnouncementsByTags from 'models/announcement/operations/get-pinned-announcements-by-tags.js';
 
@@ -120,6 +121,27 @@ apis.get( '/tags-pinned', cors(), async ( req, res, next ) => {
         res.json( await getPinnedAnnouncementsByTags( {
             from:       new Date( Number( req.query.from ) ),
             languageId: Number( req.query.languageId ),
+            tags,
+            to:         new Date( Number( req.query.to ) ),
+        } ) );
+    }
+    catch ( error ) {
+        next( error );
+    }
+} );
+
+apis.get( '/get-hot-announcements', cors(), async ( req, res, next ) => {
+    try {
+        let tags = req.query.tags || [];
+        if ( !Array.isArray( tags ) )
+            tags = [ tags, ];
+        tags = tags.map( Number );
+
+        res.json( await getHotAnnouncements( {
+            amount:     Number( req.query.amount ),
+            from:       new Date( Number( req.query.from ) ),
+            languageId: Number( req.query.languageId ),
+            page:       Number( req.query.page ),
             tags,
             to:         new Date( Number( req.query.to ) ),
         } ) );
