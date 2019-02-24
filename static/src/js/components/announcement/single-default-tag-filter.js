4a9416d4e0bdb734cb1a3cca12429b3781c6e1ff
Kevin <ProFatXuanAll@gmail.com>
2019-02-25 00:15:59 +0800

    revised default-tag-filter.js

--- static/src/js/components/announcement/single-default-tag-filter.js
+++ static/src/js/components/announcement/single-default-tag-filter.js
@@ -1,51 +1,7 @@
 import { classAdd, classRemove, } from 'static/src/js/utils/style.js';
 import DefaultTagFilter from 'static/src/js/components/announcement/default-tag-filter.js';
-import ValidateUtils from 'models/common/utils/validate.js';
 
 export default class SingleDefaultTagFilter extends DefaultTagFilter {
-    subscribeTimeEvent () {
-        [
-            'from',
-            'to',
-        ].forEach( ( timeFilter ) => {
-            [
-                'year',
-                'month',
-                'date',
-            ].forEach( ( timePart ) => {
-                this.DOM.filter[ timeFilter ][ timePart ].addEventListener( 'change', () => {
-                    try {
-                        const year  = this.DOM.filter[ timeFilter ].year.value;
-                        const month = this.DOM.filter[ timeFilter ].month.value;
-                        const date  = this.DOM.filter[ timeFilter ].date.value;
-                        this.state.page = this.config.page;
-                        this.state[ timeFilter ] = new Date( `${ year }/${ month }/${ date }` );
-
-                        if ( !ValidateUtils.isValidDate( this.state[ timeFilter ] ) ) {
-                            this.DOM.announcement.pinned.briefings.innerHTML = '';
-                            classAdd( this.DOM.announcement.pinned.loading, 'loading--hidden' );
-                            classRemove( this.DOM.announcement.pinned.noResult, 'no-result--hidden' );
-                            this.DOM.announcement.normal.briefings.innerHTML = '';
-                            classAdd( this.DOM.announcement.normal.loading, 'loading--hidden' );
-                            classRemove( this.DOM.announcement.normal.noResult, 'no-result--hidden' );
-                            throw new TypeError( 'invalid arguments' );
-                        }
-
-                        if ( this.isLocked() )
-                            return;
-                        this.acquireLock();
-
-                        this.pushState();
-                        this.getAll();
-                    }
-                    catch ( err ) {
-                        console.error( err );
-                    }
-                } );
-            } );
-        } );
-    }
-
     subscribeTagEvent () {
         this.DOM.filter.tags.forEach( ( tagObj ) => {
             /**
@@ -59,46 +15,57 @@ export default class SingleDefaultTagFilter extends DefaultTagFilter {
 
                 classAdd( tagObj.node, 'tags__tag--active' );
                 tagObj.node.addEventListener( 'click', () => {
-                    if ( this.isLocked() )
-                        return;
-                    this.acquireLock();
+                    try {
+                        if ( this.isLocked() )
+                            return;
+                        this.acquireLock();
 
-                    this.DOM.filter.tags.forEach( ( tagObj ) => {
-                        classRemove( tagObj.node, 'tags__tag--active' );
-                    } );
+                        this.DOM.filter.tags.forEach( ( tagObj ) => {
+                            classRemove( tagObj.node, 'tags__tag--active' );
+                        } );
 
-                    classAdd( tagObj.node, 'tags__tag--active' );
+                        classAdd( tagObj.node, 'tags__tag--active' );
 
-                    this.state.page = this.config.page;
-                    this.state.tags = [];
+                        this.state.page = this.config.page;
+                        this.state.tags = [];
 
-                    this.pushState();
-                    this.getAll();
+                        this.pushState();
+                        this.getAll();
+                    }
+                    catch ( err ) {
+                        console.error( err );
+                    }
                 } );
             }
             else {
                 tagObj.node.addEventListener( 'click', () => {
-                    if ( this.isLocked() )
-                        return;
-                    this.acquireLock();
+                    try {
+                        if ( this.isLocked() )
+                            return;
+                        this.acquireLock();
 
-                    const index = this.state.tags.indexOf( tagObj.id );
-                    if ( index >= 0 ) {
-                        this.state.tags.splice( index, 1 );
-                        classRemove( tagObj.node, 'tags__tag--active' );
+                        const index = this.state.tags.indexOf( tagObj.id );
+
+                        if ( index >= 0 ) {
+                            this.state.tags.splice( index, 1 );
+                            classRemove( tagObj.node, 'tags__tag--active' );
+                        }
+                        else {
+                            this.state.tags.push( tagObj.id );
+                            classAdd( tagObj.node, 'tags__tag--active' );
+                        }
+
+                        this.state.page = this.config.page;
+
+                        if ( this.state.tags.length === 0 )
+                            this.state.tags = [];
+
+                        this.pushState();
+                        this.getAll();
                     }
-                    else {
-                        this.state.tags.push( tagObj.id );
-                        classAdd( tagObj.node, 'tags__tag--active' );
+                    catch ( err ) {
+                        console.error( err );
                     }
-
-                    this.state.page = this.config.page;
-
-                    if ( this.state.tags.length === 0 )
-                        this.state.tags = [];
-
-                    this.pushState();
-                    this.getAll();
                 } );
             }
         } );
