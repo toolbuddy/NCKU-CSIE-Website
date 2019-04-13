c19b822f31358f8afcbeacc41c4b472a81f88e74
ccs100203 <ccs100203@gmail.com>
2019-04-13 23:59:10 +0800

    Add Publication

--- static/src/js/research/publication.js
+++ static/src/js/research/publication.js
@@ -2,6 +2,8 @@ import GetHeaderBase from 'static/src/js/components/common/header-base.js';
 import GetHeaderSmall from 'static/src/js/components/common/header-small.js';
 import GetHeaderMedium from 'static/src/js/components/common/header-medium.js';
 import GetHeaderLarge from 'static/src/js/components/common/header-large.js';
+import GetPublications from 'static/src/js/components/research/publication/get-publication.js';
+import WebLanguageUtils from 'static/src/js/utils/language.js';
 
 try {
     const headerBase = new GetHeaderBase( {
@@ -42,6 +44,17 @@ try {
     } );
     if ( !( headerLarge instanceof GetHeaderLarge ) )
         throw new Error( '.header.header--medium not found.' );
+}
+catch ( err ) {
+    console.error( err );
+}
+try {
+    const getPublications = new GetPublications( {
+        publicationDOM: document.getElementById( 'publication' ),
+        languageId:     WebLanguageUtils.currentLanguageId,
+    } );
+
+    getPublications.exec();
 }
 catch ( err ) {
     console.error( err );
