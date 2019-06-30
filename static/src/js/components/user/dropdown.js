7b72d59fb2202d91ee0156dbc2a3f4d0998772bf
林謙慈 <amylin19990210@gmail.com>
2019-06-30 21:27:03 +0800

    大改版

--- static/src/js/components/user/dropdown.js
+++ static/src/js/components/user/dropdown.js
@@ -26,9 +26,10 @@ export default class DropdownControl {
             const block        = element.target.parentNode.parentNode;
             block.firstChild.classList.remove( 'content__button--top' );
 
-            const blockNode    = block.cloneNode( true );
-            const selectedClass   = element.target.parentNode.className;
-            const selectedNode = block.getElementsByClassName( selectedClass )[ 0 ].cloneNode( true );
+            const blockNode     = block.cloneNode( true );
+            const selectedClass = element.target.parentNode.className;
+            const selectedNode  = block.getElementsByClassName( selectedClass )[ 0 ].cloneNode( true );
+            console.log( selectedNode );
             selectedNode.classList.add( 'content__button--top' );
 
             blockNode.removeChild( blockNode.getElementsByClassName( selectedClass )[ 0 ] );
--- static/src/js/user/project.js
+++ static/src/js/user/project.js
@@ -1,6 +1,7 @@
 import GetHeaderBase from 'static/src/js/components/common/header-base.js';
 import GetHeaderMedium from 'static/src/js/components/common/header-medium.js';
 import GetHeaderLarge from 'static/src/js/components/common/header-large.js';
+import BlockControl from 'static/src/js/components/user/block-control.js';
 
 try {
     const headerBase = new GetHeaderBase( {
@@ -34,3 +35,15 @@ try {
 catch ( err ) {
     console.error( err );
 }
+
+try {
+    const blockControl = new BlockControl();
+    if ( !( blockControl instanceof BlockControl ) )
+        throw new Error( 'titleBlockControl not founs' );
+
+    blockControl.addButtonEvent( document.getElementById( 'project__input-button--add' ) );
+}
+catch ( err ) {
+    console.error( err );
+}
+
