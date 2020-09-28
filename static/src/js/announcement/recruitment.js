7432074ec2203cf0021054e1acc7ac187de524f1
林謙慈 <amylin19990210@gmail.com>
2020-09-28 20:09:52 +0800

    move user/id into announcement componment

--- static/src/js/announcement/recruitment.js
+++ static/src/js/announcement/recruitment.js
@@ -3,8 +3,6 @@ import WebLanguageUtils from 'static/src/js/utils/language.js';
 import GetHeaderBase from 'static/src/js/components/common/header-base.js';
 import GetHeaderMedium from 'static/src/js/components/common/header-medium.js';
 import GetHeaderLarge from 'static/src/js/components/common/header-large.js';
-import roleUtils from 'models/auth/utils/role.js';
-import { host, } from 'settings/server/config.js';
 
 try {
     const headerBase = new GetHeaderBase( {
@@ -40,55 +38,26 @@ catch ( err ) {
     console.error( err );
 }
 
-async function fetchData () {
-    try {
-        const res = await fetch( `${ host }/user/id`, {
-            credentials: 'include',
-            method:      'post',
-        } );
-
-        if ( !res.ok )
-            throw new Error( 'No faculty found' );
-
-        return res.json();
-    }
-    catch ( err ) {
-        throw err;
-    }
+try {
+    const filter = new SingleDefaultTagFilter( {
+        defaultTag:            [ 'recruitment', ],
+        supportedTag:          [],
+        filterDOM:             document.getElementById( 'filter' ),
+        announcementPinnedDOM: document.getElementById( 'announcement--pinned' ),
+        announcementNormalDOM: document.getElementById( 'announcement--normal' ),
+        addDOM:                document.getElementById( 'announcement--add' ),
+        deletePreviewDOM:      document.getElementById( 'announcement--delete' ),
+        pagesDOM:              document.getElementById( 'pages' ),
+        scrollTopDOM:          document.getElementById( 'announcement--normal' ),
+        amount:                15,
+        from:                  new Date( '2018/01/01' ),
+        to:                    new Date( Date.now() ),
+        page:                  1,
+        visiblePageNum:        2,
+        currentLanguageId:     WebLanguageUtils.currentLanguageId,
+    } );
+    filter.getAll();
 }
-
-( async () => {
-    try {
-        const result = await fetchData();
-        const userRole = result.role;
-        const userId =   ( userRole === roleUtils.getIdByOption( 'staff' ) ) ? result.roleId : -1;
-
-        try {
-            const filter = new SingleDefaultTagFilter( {
-                defaultTag:            [ 'recruitment', ],
-                supportedTag:          [],
-                filterDOM:             document.getElementById( 'filter' ),
-                announcementPinnedDOM: document.getElementById( 'announcement--pinned' ),
-                announcementNormalDOM: document.getElementById( 'announcement--normal' ),
-                addDOM:                document.getElementById( 'announcement--add' ),
-                deletePreviewDOM:      document.getElementById( 'announcement--delete' ),
-                pagesDOM:              document.getElementById( 'pages' ),
-                scrollTopDOM:          document.getElementById( 'announcement--normal' ),
-                amount:                15,
-                from:                  new Date( '2018/01/01' ),
-                to:                    new Date( Date.now() ),
-                page:                  1,
-                visiblePageNum:        2,
-                userId,
-                currentLanguageId:     WebLanguageUtils.currentLanguageId,
-            } );
-            filter.getAll();
-        }
-        catch ( err ) {
-            console.error( err );
-        }
-    }
-    catch ( err ) {
-        console.error( err );
-    }
-} )();
+catch ( err ) {
+    console.error( err );
+}
